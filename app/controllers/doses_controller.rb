d1dc021535b116902532f74f7f91cb35f0ce62e6
Andy Leong <mindreko@gmail.com>
2020-05-04 00:17:23 +1000

    fixed up index

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,7 +1,4 @@
-# frozen_string_literal: true
-
 class DosesController < ApplicationController
-  before_action :set_dose, only: [:destroy]
 
   def new
     @cocktail = Cocktail.find(params[:cocktail_id])
@@ -9,27 +6,26 @@ class DosesController < ApplicationController
   end
 
   def create
-    @cocktail = Cocktail.find(params[:cocktail_id])
-    @ingredient = Ingredient.find(params[:dose][:ingredient_id])
     @dose = Dose.new(dose_params)
+    @cocktail = Cocktail.find(params[:cocktail_id])
     @dose.cocktail = @cocktail
-    @dose.ingredient = @ingredient
-    @dose.save
-    redirect_to cocktail_path(@cocktail)
+    if @dose.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render "new"
+    end
   end
 
   def destroy
+    @dose = Dose.find(params[:id])
     @dose.destroy
-    redirect_to cocktails_path
+    redirect_to cocktails_path(@dose.cocktail)
   end
 
   private
 
   def dose_params
-    params.require(:dose).permit(:description)
+    params.require(:dose).permit(:description, :ingredient_id)
   end
 
-  def set_dose
-    @dose = Dose.find_by(params[:description])
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
-# frozen_string_literal: true
-
 Rails.application.routes.draw do
   resources :cocktails, only: %i[index show new create] do
     resources :doses, only: %i[new create]
   end
+  resources :doses, only: %i[destroy]
 end
